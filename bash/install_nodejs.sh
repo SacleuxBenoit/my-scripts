c7d9e421f56a20d0ab7b3965ee60f1400c5dcd34
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-05-02 14:47:08 +0200

    create folder bash with install_nodejs.sh

---
+++ bash/install_nodejs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Installation d'Express
+npm install express --save
+
+# Installation de Nodemon
+npm install nodemon --save-dev
+
+# Installation de Dotenv
+npm install dotenv --save
+
+# Installation de Mongoose
+npm install mongoose --save
